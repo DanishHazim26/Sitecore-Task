4f66619c145a631173aab9a55c9968695f305079
Danish Hazim <135487291+DanishHazim26@users.noreply.github.com>
2023-06-13 00:48:31 +0800

    Update MinefieldPathFinder.java

--- Task 3 (Mine Field)/MinefieldPathFinder.java
+++ Task 3 (Mine Field)/MinefieldPathFinder.java
@@ -1,69 +1,133 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class MinefieldPathFinder {
-    private int[][] minefield;  // 2D array representing the minefield
-    private int rows;           // Number of rows in the minefield
-    private int columns;        // Number of columns in the minefield
+public class MinefieldGenerator {
+    public static void main(String[] args) {
+        // Set the size of the minefield
+        int rows = 6;
+        int columns = 10;
 
-    // Constructor to initialize the minefield and its dimensions
-    public MinefieldPathFinder(int[][] minefield) {
-        this.minefield = minefield;
-        this.rows = minefield.length;
-        this.columns = minefield[0].length;
+        // Generate the minefield
+        int[][] minefield = generateMinefield(rows, columns);
+
+        // Print out the minefield to the console
+        printMinefield(minefield);
+
+        // Instantiate a path finder with the generated minefield
+        MinefieldPathFinder pathFinder = new MinefieldPathFinder(minefield);
+
+        // Find a safe path through the minefield for Totoshka
+        List<int[]> pathTotoshka = pathFinder.findPath();
+        if (pathTotoshka != null) {
+            System.out.println("Started before Ally");
+            for (int i = 0; i < pathTotoshka.size(); i++) {
+                int[] positionTotoshka = pathTotoshka.get(i);
+                System.out.print("Totoshka: Row: " + positionTotoshka[0] + ", Column: " + positionTotoshka[1]);
+
+                if (i > 0) {
+                    int[] positionAlly = pathTotoshka.get(i - 1);
+                    System.out.println(" ... Ally: Row: " + positionAlly[0] + ", Column: " + positionAlly[1]);
+                } else {
+                    System.out.println(" ... Ally: Not started yet");
+                }
+            }
+            System.out.println("Totoshka: Finished ... Ally: Row: " + pathTotoshka.get(pathTotoshka.size() - 1)[0] 
+                               + ", Column: " + pathTotoshka.get(pathTotoshka.size() - 1)[1]);
+        } else {
+            System.out.println("No safe path found for Totoshka.");
+            System.out.println("No safe path found for Ally.");
+        }
     }
 
-    // Public method to find a safe path through the minefield
-    public List<int[]> findPath() {
-        List<int[]> path = new ArrayList<>();
-        int[] currentPosition = findStart();  // Find the starting position
+    public static int[][] generateMinefield(int rows, int columns) 
+    {
+        int[][] minefield = new int[rows][columns];
+        Random random = new Random();
 
-        // If there's no valid start, return null (no safe path)
-        if (currentPosition == null) {
-            return null;
+        int[] safeColumnsPrevRow = null ;
+        int[] safeColumnsThisRow;
+
+        // For each row, determine the number of safe spots and their locations
+        for (int row = 0; row < rows; row++) 
+        {
+            //create random number for each row to insert the safe spot, if 3, then that row have 3 safe spot
+            int numSafeSpots = random.nextInt(columns) + 1;
+
+            //to store the value of safe spot in this row
+            safeColumnsThisRow = new int[numSafeSpots];
+
+            // Create safe spots, ensuring each is adjacent to at least one safe spot from the previous row
+            for (int i = 0; i < numSafeSpots; i++) 
+            {
+                int colToPlaceSafeSpot;
+
+                do {
+                    //create random int 
+                    colToPlaceSafeSpot = random.nextInt(columns);
+
+                } while (!isValidSafeSpot(row, colToPlaceSafeSpot, safeColumnsPrevRow));
+
+                //insert safe spot value to the particular row and column that safe
+                minefield[row][colToPlaceSafeSpot] = 1;
+
+                //store inside the safe columns total
+                safeColumnsThisRow[i] = colToPlaceSafeSpot;
+
+                //repeat until all rows have their own safe spot(s)
+            }
+
+            //pass it to the previous row 
+            safeColumnsPrevRow = safeColumnsThisRow;
         }
-        path.add(currentPosition);  // Add the starting position to the path
 
-        // Continue finding next safe positions until reaching the final row
-        while (currentPosition[0] < rows - 1) {
-            int[] nextPosition = findNext(currentPosition);
+        // For each spot that isn't a safe spot, place a bomb
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                if (minefield[row][col] != 1) {
+                    minefield[row][col] = 0;
+                }
+            }
+        }
+
+        return minefield;
+    }
+
+    // Check if the spot is valid as a safe spot. A spot is valid if it's adjacent to at least one safe spot from the previous row.
+    public static boolean isValidSafeSpot(int row, int col, int[] safeColumnsIndex) {
+       
+        if (row == 0) {
+            return true; // First row doesn't need to check connectivity
+        }
+
+        //safeColumns total, example if 3 safeColumns random generated, then only 3 safe spot can be input
+        for (int i = 0; i < safeColumnsIndex.length; i++) 
+        {
+            int prevRow = row - 1; //is current row of that minefield
+            int prevCol = safeColumnsIndex[i]; //initialized by getting safeColumns 
             
-            // If there's no valid next position, return null (no safe path)
-            if (nextPosition == null) {
-                return null;
+            if (isAdjacent(prevRow, prevCol, row, col)) {
+                return true;
             }
-            path.add(nextPosition);  // Add the next position to the path
-            currentPosition = nextPosition;  // Update current position
         }
 
-        return path;  // Return the found safe path
+        return false;
     }
 
-    // Private helper method to find a safe starting position in the first row
-    private int[] findStart() {
-        for (int i = 0; i < columns; i++) {
-            // If a safe spot (1) is found, return its coordinates
-            if (minefield[0][i] == 1) {
-                return new int[]{0, i};
-            }
-        }
-        // If no safe spot found in the first row, return null
-        return null;
+    // Check if two spots are adjacent. Two spots are adjacent if they are at most one row and one column apart.
+    public static boolean isAdjacent(int row1, int col1, int row2, int col2) 
+    {
+        //return true if value either 0 or 1
+        return Math.abs(row1 - row2) <= 1 && Math.abs(col1 - col2) <= 1;
     }
 
-    // Private helper method to find a safe next position from the current one
-    private int[] findNext(int[] currentPosition) {
-        int row = currentPosition[0];
-        int col = currentPosition[1];
-
-        // Check all possible adjacent spots in the next row
-        for (int j = Math.max(0, col - 1); j <= Math.min(columns - 1, col + 1); j++) {
-            // If a safe spot (1) is found, return its coordinates
-            if (minefield[row + 1][j] == 1) {
-                return new int[]{row + 1, j};
+    // Print the minefield to the console. Safe spots are represented by '1', bomb spots by '0'.
+    public static void printMinefield(int[][] minefield) {
+        for (int row = 0; row < minefield.length; row++) {
+            for (int col = 0; col < minefield[row].length; col++) {
+                System.out.print(minefield[row][col] + " ");
             }
+            System.out.println();
         }
-        // If no safe spot found, return null
-        return null;
     }
 }
+
